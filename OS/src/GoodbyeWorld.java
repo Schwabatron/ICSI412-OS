b4eb28c8bc8d0e9281ec19cd07edd8c64b1fecdd
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-02-08 17:10:44 -0500

    completed the GoodbyeWorld.java and HelloWorld.java classes. also moved the call to main() in Process.java inside the try catch block rather than outside it

--- OS/src/GoodbyeWorld.java
+++ OS/src/GoodbyeWorld.java
@@ -1,2 +1,13 @@
-public class GoodbyeWorld { //Create main method that makes an infinite loop that just prints goodbye world make sure that each calls cooperate and thread.sleep(50)
+public class GoodbyeWorld extends UserlandProcess{ //Create main method that makes an infinite loop that just prints goodbye world make sure that each calls cooperate and thread.sleep(50)
+    @Override
+    public void main() {
+        while(true)
+        {
+            try {
+                cooperate();
+                Thread.sleep(50);
+                System.out.println("Goodbye World!");
+            } catch (Exception e) { }
+        }
+    }
 }
--- OS/src/HelloWorld.java
+++ OS/src/HelloWorld.java
@@ -1,2 +1,13 @@
-public class HelloWorld { //Create main method that makes an infinite loop that just prints hello world make sure that each calls cooperate and thread.sleep(50)
+public class HelloWorld extends UserlandProcess { //Create main method that makes an infinite loop that just prints hello world make sure that each calls cooperate and thread.sleep(50)
+    @Override
+    public void main() {
+        while(true)
+        {
+            try {
+                cooperate();
+                Thread.sleep(50);
+                System.out.println("Hello World!");
+            } catch (Exception e) { }
+        }
+    }
 }
