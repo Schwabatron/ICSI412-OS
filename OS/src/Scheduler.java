14b63131f0cdfd11b08ec9fbb06e6b4b49f01923
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-04-03 17:40:24 -0400

    smoothed out some repeated code into methods

--- OS/src/Scheduler.java
+++ OS/src/Scheduler.java
@@ -68,12 +68,7 @@ public class Scheduler {
 
     public int CreateProcess(UserlandProcess up, OS.PriorityType p) {
         PCB new_process = new PCB(up, p);
-        switch(p) //adding to the correct priority queue
-        {
-            case OS.PriorityType.background -> background_processes.add(new_process);
-            case OS.PriorityType.realtime -> realtime_processes.add(new_process);
-            case OS.PriorityType.interactive -> interactive_processes.add(new_process);
-        }
+        add_to_queue(new_process);
         //Not sure how to check if nothing else is running
         if(current_process == null)
         {
@@ -88,11 +83,7 @@ public class Scheduler {
 
     public void switchProcess() {
         if (current_process != null && !current_process.isDone()) { //if current process is not null and not done
-            switch(current_process.getPriority()) {
-                case OS.PriorityType.background -> background_processes.add(current_process);
-                case OS.PriorityType.realtime -> realtime_processes.add(current_process);
-                case OS.PriorityType.interactive -> interactive_processes.add(current_process);
-            }
+            add_to_queue(current_process);
         }
         else if(current_process != null && current_process.isDone()) {
             Exit();
@@ -114,17 +105,7 @@ public class Scheduler {
 
         for (PCB process : sleeping_processes) { //iterate through sleepers
             if (process.wake_up_time <= current_time) { //if the wake up time is less than the current time then wake them up
-                switch (process.getPriority()) {
-                    case OS.PriorityType.background ->{
-                        background_processes.add(process);
-                    }
-                    case OS.PriorityType.realtime -> {
-                        realtime_processes.add(process);
-                    }
-                    case OS.PriorityType.interactive -> {
-                        interactive_processes.add(process);
-                    }
-                }
+                add_to_queue(process);
                 processes_to_be_awoken.add(process);
             }
         }
@@ -200,16 +181,11 @@ public class Scheduler {
                 if(km.receiver_PID == process.pid) { //if we find the process that we would like to send the message to
                     //System.out.println("kernel message added to queue for process: " + process.getName());
                     process.message_Queue.add(km); //add the message to the message queue
-                    if(waiting_processes.contains(process)) { //checking if this process was waiting on a message
+                    if(queue == waiting_processes) { //checking if this process was waiting on a message
                         waiting_processes.remove(process); //remove from the waiting process queue
                         process.woken_up = true;
                         //System.out.println("process removed from the waiting queue: " + process.getName());
-                        switch(process.getPriority()) //add it back to its correct priority queue
-                        {
-                            case OS.PriorityType.background -> background_processes.add(process);
-                            case OS.PriorityType.realtime -> realtime_processes.add(process);
-                            case OS.PriorityType.interactive -> interactive_processes.add(process);
-                        }
+                        add_to_queue(process);
                     }
                 }
             }
@@ -229,5 +205,15 @@ public class Scheduler {
         }
 
         return null; //not sure what to return
+    }
+
+
+    private void add_to_queue(PCB process) {
+        switch(process.getPriority()) //add it back to its correct priority queue
+        {
+            case OS.PriorityType.background -> background_processes.add(process);
+            case OS.PriorityType.realtime -> realtime_processes.add(process);
+            case OS.PriorityType.interactive -> interactive_processes.add(process);
+        }
     }
 }
